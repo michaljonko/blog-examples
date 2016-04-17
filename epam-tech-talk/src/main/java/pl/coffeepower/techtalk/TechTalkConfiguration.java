1d338f41ab2975d766ae9795ee04870d5577665e
Michal Jonko <michal.jonko@gmail.com>
2016-04-17 15:29:43 +0200

    parameters fix

--- epam-tech-talk/src/main/java/pl/coffeepower/techtalk/TechTalkConfiguration.java
+++ epam-tech-talk/src/main/java/pl/coffeepower/techtalk/TechTalkConfiguration.java
@@ -24,67 +24,31 @@
 
 package pl.coffeepower.techtalk;
 
-import com.google.common.net.InetAddresses;
-
-import com.beust.jcommander.IParameterValidator;
 import com.beust.jcommander.Parameter;
-import com.beust.jcommander.ParameterException;
-import com.beust.jcommander.validators.PositiveInteger;
-
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
 
 import pl.coffeepower.blog.messagebus.Configuration;
 import pl.coffeepower.blog.messagebus.Engine;
 
 import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class TechTalkConfiguration implements Configuration {
 
-    @Parameter(names = "topicId", validateWith = PositiveInteger.class)
+    @Parameter(names = "--topicId", arity = 1, required = false)
     int topicId = 1;
-    @Parameter(names = "multicastAddr", validateWith = MulticastAddressValidator.class)
+    @Parameter(names = "--multicastAddr", arity = 1, required = false)
     String multicastAddress = "225.0.0.123";
-    @Parameter(names = "multicastPort", validateWith = PositiveInteger.class)
+    @Parameter(names = "--multicastPort", arity = 1, required = false)
     int multicastPort = 12345;
-    @Parameter(names = "ifaceAddr", validateWith = IPv4AddressValidator.class)
+    @Parameter(names = "--ifaceAddr", arity = 1, required = false)
     String bindAddress = InetAddress.getLoopbackAddress().getHostAddress();
-    @Parameter(names = "engine")
+    @Parameter(names = "--engine", arity = 1, required = false)
     Engine engine = Engine.FAST_CAST;
-    @Parameter(names = "packets", validateWith = PositiveInteger.class)
+    @Parameter(names = "--packets", arity = 1, required = false)
     int packets = 1_000_000;
-
-    private static final class MulticastAddressValidator implements IParameterValidator {
-
-        @Override
-        public void validate(String name, @NonNull String value) throws ParameterException {
-            try {
-                if (!InetAddresses.forString(value).isMulticastAddress()) {
-                    throw new ParameterException("Parameter " + name + " is not proper multicast address (found " + value + ").");
-                }
-            } catch (IllegalArgumentException e) {
-                throw new ParameterException("Parameter " + name + " is not proper IP address (found " + value + ").");
-            }
-        }
-    }
-
-    private static final class IPv4AddressValidator implements IParameterValidator {
-
-        @Override
-        public void validate(String name, @NonNull String value) throws ParameterException {
-            try {
-                if (!InetAddresses.isInetAddress(value) || !NetworkInterface.getByInetAddress(InetAddresses.forString(value)).isUp()) {
-                    throw new ParameterException("Parameter " + name + " is not proper interface address or interface is down (found " + value + ").");
-                }
-            } catch (IllegalArgumentException | SocketException e) {
-                throw new ParameterException("Parameter " + name + " is not proper interface IP address (found " + value + ").");
-            }
-        }
-    }
 }
